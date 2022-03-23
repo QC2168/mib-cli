03e90f319462214e8f3e709c1e7aeb098e19b3ee
QC2168 <huangxinhong2125@gmail.com>
2022-03-23 23:59:15 +0800

    multiple device list

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 /* eslint-disable no-restricted-syntax */
-import { execSync } from "child_process";
 import fs from "fs-extra";
 import { FileNodeType, SaveItemType } from "./types";
 import {
-  diff, log, isPath, replace,
+  diff,
+  log,
+  isPath,
+  replace,
+  execAdb,
+  isPathAdb,
+  selectDevice,
 } from "./utils";
 import { getConfig } from "./config";
 
@@ -12,20 +17,17 @@ const speedReg: RegExp = /[0-9.]+\s(MB\/s)/;
 // 获取手机中文件的大小
 const getFileSize = (path: string): number => {
   try {
-    const res = execSync(
-      `adb shell du -k "${replace(path)}" | adb shell cut -f 1`,
-    ).toString();
-    return Number(res);
+    const res = execAdb(`shell du -k "${replace(path)}"`).toString();
+    const fileSize: string = res.split("\t")[0];
+    return Number(fileSize);
   } catch (error) {
-    log(`获取文件大小失败-${path}`, 'warn');
+    log(`获取文件大小失败-${path}`, "warn");
     return 0;
   }
 };
 
 const getCurFileList = (path: string) => {
-  const res: string[] = execSync(`adb shell ls -l ${path}`)
-    .toString()
-    .split("\r\n");
+  const res: string[] = execAdb(`shell ls -l ${path}`).toString().split("\r\n");
   // 去除开头的total
   res.shift();
   // 去除最后一个字符串
@@ -93,9 +95,9 @@ const move = (backupQueue: FileNodeType[], outputDir: string): void => {
   for (const fileN of backupQueue) {
     log(`正在备份${fileN.fileName}`);
     try {
-      const out: string = execSync(
-        `adb pull "${fileN.filePath}" "${outputDir + fileN.fileName}"`,
-      ).toString();
+      const out: string = execAdb(
+        `pull "${fileN.filePath}" "${outputDir + fileN.fileName}"`,
+      );
       const speed: string | null = out.match(speedReg) !== null ? out.match(speedReg)![0] : "读取速度失败";
       log(`平均传输速度${speed}`);
     } catch (e: any) {
@@ -123,14 +125,22 @@ const MIB = () => {
     log(`当前执行备份任务:${item.comment}`);
     const arr = item.path.split("/").filter((i: string) => i !== "");
     const folderName = arr[arr.length - 1];
-    // 拼接导出路径
-    const outputDir = `${output + folderName}/`;
-    // 判断导出路径
-    isPath(outputDir);
     const backupDir = item.path;
-    backup(backupDir, outputDir);
+    // 判断备份路径是否存在
+    if (!isPathAdb(backupDir)) {
+      log(`备份路径:${backupDir} 不存在已跳过`, "error");
+    } else {
+      // 拼接导出路径
+      const outputDir = `${output + folderName}/`;
+      // 判断导出路径
+      isPath(outputDir);
+      backup(backupDir, outputDir);
+    }
   });
   log("程序结束");
 };
 
-MIB();
+(async () => {
+  await selectDevice();
+  MIB();
+})();
---
+++ src/types.ts
@@ -0,0 +1,26 @@
+// 文件节点
+export interface FileNodeType {
+  fileSize: number;
+  fileName: string;
+  filePath: string;
+}
+
+// 备份节点
+export interface SaveItemType {
+  path: string;
+  comment: string;
+  full?: boolean;
+  output?:string;
+}
+
+// 备份目标
+export interface BackupsType {
+  backups: SaveItemType[];
+  output: string;
+}
+
+// 设备列表
+export interface devicesType {
+  name: string;
+  status: string;
+}
