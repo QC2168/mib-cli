fa5e888d1568b17d60a15ae251ee65ffb8fb9406
QC2168 <huangxinhong2125@gmail.com>
2022-12-25 00:03:04 +0800

    :bug: export types

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './backup';
 export * from './config';
 export * from './devices';
 export * from './node';
+export * from './types';
 export { default as isPath } from './utils/isPath';
 export { default as pathRepair } from './utils/pathRepair';
 export * from './utils/adb';
